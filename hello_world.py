7dd5874ac01220f3b2520b657ceff14b499def5e
Arbaaz Khan <arbaazkhan@gmail.com>
2018-12-15 14:41:53 +0530

    Added hello world program.

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+
+''' *****************************************************************************
+ *  Name:      Arbaaz Khan
+ *  Language:  python3 
+ *
+ *  Description:  Prints 'Hello, World' to the terminal window.
+ *
+ *  Written:       15/12/2018
+ *  Last updated:  15/12/2018
+ *
+ *  % python hello_world.py
+ *  Hello, World!
+ *
+***************************************************************************** '''
+print("Hello, World!")
