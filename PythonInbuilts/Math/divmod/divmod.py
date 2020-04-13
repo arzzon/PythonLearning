502a2a7fc80780ea3c94ef9ab7f055c689bf27ad
Arbaaz Khan <arbaazkhan@gmail.com>
2020-04-13 10:34:38 +0530

    Added divmod

---
+++ PythonInbuilts/Math/divmod/divmod.py
@@ -0,0 +1,9 @@
+'''
+divmod operators on two numbers and returns the quotient and remainder of the numbers
+quotient, remainder = divmod(n,m)
+'''
+q, r = divmod(25, 10)
+print(q,r)
+#   Q       R
+# 25//10  25%10
+#   2       5
