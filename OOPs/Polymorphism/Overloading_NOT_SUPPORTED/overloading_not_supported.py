a2e097255825ccb9bdd3ca4b674c93c32bc33bb4
Arbaaz Khan <arbaazkhan@gmail.com>
2020-04-17 09:20:38 +0530

    Added some python OOPs concepts.

---
+++ OOPs/Polymorphism/Overloading_NOT_SUPPORTED/overloading_not_supported.py
@@ -0,0 +1,3 @@
+print('python does not supports method overloading. \
+We may overload the methods but can only use \
+the latest defined method.')
---
+++ OOPs/Polymorphism/Overriding/overriding.py
@@ -0,0 +1,24 @@
+'''
+    Method overriding is supported in python. The overridden method is called via it's object,
+    similarly the parent class object that is overridden can be called by parent class object.
+'''
+
+
+class Rectangle():
+  def __init__(self,length,breadth):
+    self.length = length
+    self.breadth = breadth
+  def getArea(self):
+    print(self.length*self.breadth," is area of rectangle")
+
+class Square(Rectangle):
+  def __init__(self,side):
+    self.side = side
+    Rectangle.__init__(self,side,side)
+  def getArea(self):
+    print(self.side*self.side," is area of square")
+
+s = Square(4)
+r = Rectangle(2,4)
+s.getArea()
+r.getArea()
