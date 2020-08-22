3cfb5f4c5b06ad32770c304aaf2573c68507f266
Arbaaz Khan <arbaazkhan@gmail.com>
2020-08-22 21:20:26 +0530

    Added insort function example in bisect library example program.

--- PythonInbuilts/Bisect/bisect_example.py
+++ PythonInbuilts/Bisect/bisect_example.py
@@ -4,6 +4,14 @@ import bisect
 # to insert, return the position where it should be inserted, if the number already exists
 # then it returns the position right to it(if we want the left position then use bisect_left()).
 # It uses binary search
+# bisect and bisect_right() do the same thing
 print(bisect.bisect([0, 1, 2, 3, 4, 5, 6, 7], 4))  # 5
 print(bisect.bisect_left([0, 1, 2, 3, 4, 5, 6, 7], 4))  # 4
 print(bisect.bisect([0, 1, 2, 3, 5, 6, 7], 4))  # 4
+
+# If we want to find as well as insert the number in the correct position in the list then use
+# insort(), similarly we have insort_right() and insort_left()
+A = [0, 1, 2, 3, 5, 6, 7]
+bisect.insort(A,4)
+print(A)  # [0, 1, 2, 3, 4, 5, 6, 7]
+
